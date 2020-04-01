fc0018a35343ea6045977094efbec61c22842bc7
Qiushixiang <qiushixiang315@gmail.com>
2020-04-01 18:21:23 +0800

    Modify auto install shell

--- installation.sh
+++ installation.sh
@@ -116,73 +116,16 @@ install_docker
 
 install_docker_compose
 
-git clone https://github.com/Robot-Chen/trojan-caddy-docker-compose.git
+git clone https://github.com/qiushixiang/trojan-caddy-docker-compose.git
 
 cd trojan-caddy-docker-compose
 
 docker-compose down
 
-cat > ./caddy/Caddyfile <<-EOF
-${your_domain}:80 {
-    root /usr/src/trojan
-    log /usr/src/caddy.log
-    index index.html
-}
+sed -i "s/YOUR_DOMAIN_NAME/${your_domain}/g" ./caddy/Caddyfile
 
-${your_domain}:443 {
-    root /usr/src/trojan
-    log /usr/src/caddy.log
-    index index.html
-}
-EOF
-
-
-	rm -rf ./trojan/config/config.json
-	cat > ./trojan/config/config.json <<-EOF
-{
-    "run_type": "server",
-    "local_addr": "0.0.0.0",
-    "local_port": 443,
-    "remote_addr": "caddy",
-    "remote_port": 80,
-    "password": [
-        "$trojan_passwd"
-    ],
-    "log_level": 1,
-    "ssl": {
-        "cert": "/ssl/$your_domain/$your_domain.crt",
-        "key": "/ssl/$your_domain/$your_domain.key",
-        "key_password": "",
-        "cipher": "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256",
-        "prefer_server_cipher": true,
-        "alpn": [
-            "http/1.1"
-        ],
-        "reuse_session": true,
-        "session_ticket": false,
-        "session_timeout": 600,
-        "plain_http_response": "",
-        "curves": "",
-        "dhparam": ""
-    },
-    "tcp": {
-        "prefer_ipv4": false,
-        "no_delay": true,
-        "keep_alive": true,
-        "fast_open": false,
-        "fast_open_qlen": 20
-    },
-    "mysql": {
-        "enabled": false,
-        "server_addr": "127.0.0.1",
-        "server_port": 3306,
-        "database": "trojan",
-        "username": "trojan",
-        "password": ""
-    }
-}
-EOF
-	
+sed -i "s/YOUR_DOMAIN_NAME/${your_domain}/g" ./trojan/config/config.json
+sed -i "s/YOUR_PASSWORD/${trojan_passwd}/g" ./trojan/config/config.json
 
 	docker-compose up -d
 	if [ $? = 0 ]; then
